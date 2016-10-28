26046217111254df45db0be82fb99a5e334bdc53
HokopDev <hokopdev@gmail.com>
2016-10-29 00:20:18 +0200

    first commit

---
+++ src/main/java/fr/hokop/casino/Casino.java
@@ -0,0 +1,14 @@
+package fr.hokop.casino;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by vgfab on 29/10/2016.
+ */
+public class Casino extends JavaPlugin{
+
+    @Override
+    public void onDisable() {
+        System.out.println("test");
+    }
+}
